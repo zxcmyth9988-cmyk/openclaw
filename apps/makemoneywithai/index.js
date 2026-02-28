dd1b459bc68e58b211ec6c891e65c2f4d083c348
Axiom-Juans-zero <zxcmyth9988@gmail.com>
2026-02-28 00:56:46 -0800

    Change server listen address to '0.0.0.0'

--- apps/makemoneywithai/index.js
+++ apps/makemoneywithai/index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello from makemoneywithai!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
